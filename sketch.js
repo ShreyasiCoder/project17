f530cce44c0480f288690e9ef77250e1f3b6fafd
ShreyasiCoder <77727471+ShreyasiCoder@users.noreply.github.com>
2021-01-22 17:19:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var path,mainCyclist;
+var player1,player2,player3;
+var pathImg,mainRacerImg1,mainRacerImg2;
+
+
+var oppPink1Img,oppPink2Img;
+var oppYellow1Img,oppYellow2Img;
+var oppRed1Img,oppRed2Img;
+var gameOverImg,cycleBell;
+
+var pinkCG, yellowCG,redCG; 
+
+var END =0;
+var PLAY =1;
+var gameState = PLAY;
+
+var distance=0;
+var gameOver, restart;
+
+function preload(){
+  pathImg = loadImage("images/Road.png");
+  mainRacerImg1 = loadAnimation("images/mainPlayer1.png","images/mainPlayer2.png");
+  mainRacerImg2= loadAnimation("images/mainPlayer3.png");
+  
+  cycleBell = loadSound("sound/bell.mp3");
+ 
+}
+function setup(){
+  
+createCanvas(500,300);
+  
+// Moving background
+path=createSprite(100,150);
+path.addImage(pathImg);
+path.velocityX = -5;
+
+//creating boy running
+mainCyclist  = createSprite(70,150,20,20);
+mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+mainCyclist.scale=0.07;
+  
+  
+}
+
+function draw() {
+  background(0);
+  
+  drawSprites();
+  textSize(20);
+  fill(255);
+  text("Distance: "+ distance,350,30);
+  
+  if(gameState===PLAY){
+  
+   mainCyclist.y = World.mouseY;
+  
+   edges= createEdgeSprites();
+   mainCyclist .collide(edges);
+  
+  //code to reset the background
+  if(path.x < 0 ){
+    path.x = width/2;
+  }
+    
+ }
+}
